fe1f74247d231689b2556f32bc3a56b2a7828c4a
Daniel <boesch.daniel98@gmail.com>
2021-08-14 17:00:38 +0200

    add some more settings

--- particles.js
+++ particles.js
@@ -4,8 +4,15 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
 const particles = [];
 
+var enableGrowth = false;
 var enableGravity = false;
 var gravityStrength = 1;
+var particleCount = 200;
+var initialSpeed = 1;
+var randomColor = true;
+var pickedParticleColor = 'rgba(255,0,0,1)';
+
+var particleColor = 'rgba(255,0,0,1)';
 
 var mouseX = 0;
 var mouseY = 0;
@@ -18,22 +25,26 @@ function random (min, max) {
 }
 
 function draw() {
-    console.log(enableGravity);
+
+    particleColor = randomColor ?
+        `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+        : pickedParticleColor;
 
     const particle = {
         x: mouseX,
         y: mouseY,
-        xvel: random(-1,1),
-        yvel: random(-1,1),
-        color: `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`,
+        xvel: random(-1,1) * initialSpeed,
+        yvel: random(-1,1) * initialSpeed,
+        color: particleColor,
         size: 7,
+        alpha: 1
     };
 
     if (mousedown) {
         particles.push(particle);
     }
 
-    if (particles.length > 200) {
+    if (particles.length > particleCount) {
         particles.shift();
     }
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -41,6 +52,11 @@ function draw() {
     for (let i = 0; i < particles.length; i += 1){
         const p = particles[i];
         context.fillStyle = p.color;
+
+        if(enableGrowth) {
+            p.size += 0.1
+        }
+
         context.fillRect(p.x, p.y, p.size, p.size);
         p.x += p.xvel;
         p.y += p.yvel;
@@ -49,14 +65,13 @@ function draw() {
             p.yvel += 0.1 * gravityStrength;
         }
     }
-
     window.requestAnimationFrame(draw);
-
 }
 
 window.requestAnimationFrame(draw);
 
 function initEventListeners(canvas) {
+
     canvas.addEventListener("mousemove", function(e) {
         const cRect = canvas.getBoundingClientRect();        // Gets CSS pos, and width/height
         mouseX = Math.round(e.clientX - cRect.left);  // Subtract the 'left' of the canvas
@@ -65,21 +80,21 @@ function initEventListeners(canvas) {
 
     canvas.addEventListener("mousedown", function () {
         mousedown = true;
-    })
+    });
 
     canvas.addEventListener("mouseup", function () {
         mousedown = false;
-    })
+    });
 
     canvas.addEventListener("touchstart", function (e) {
         if (e.target === canvas) e.preventDefault();
         mousedown = true;
-    })
+    });
 
     canvas.addEventListener("touchend", function (e) {
         if (e.target === canvas) e.preventDefault();
         mousedown = false;
-    })
+    });
 
     canvas.addEventListener("touchmove", function (e) {
         if (e.target === canvas) e.preventDefault();
@@ -87,13 +102,46 @@ function initEventListeners(canvas) {
         const cRect = canvas.getBoundingClientRect();
         mouseX = Math.round(touch.clientX - cRect.left);
         mouseY = Math.round(touch.clientY - cRect.top);
-    })
+    });
+
+    document.getElementById("enable_growth").addEventListener("change", function () {
+        enableGrowth = !enableGrowth;
+    });
 
     document.getElementById("enable_gravity").addEventListener("change", function () {
         enableGravity = !enableGravity;
-    })
+    });
 
     document.getElementById("gravity_strength").addEventListener("change", function () {
         gravityStrength = document.getElementById("gravity_strength").value;
+    });
+
+    document.getElementById("particle_count").addEventListener("change", function () {
+        particleCount = document.getElementById("particle_count").value;
+    });
+
+    document.getElementById("initialSpeed").addEventListener("change", function () {
+        initialSpeed = document.getElementById("initialSpeed").value;
+    });
+
+    document.getElementById("random_color").addEventListener("change", function () {
+        randomColor = !randomColor;
     })
+
+    document.getElementById("particle_color").addEventListener("change", function () {
+        pickedParticleColor = hexToRgbA(document.getElementById("particle_color").value);
+    });
+}
+
+function hexToRgbA(hex){
+    var c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+        c= hex.substring(1).split('');
+        if(c.length === 3){
+            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c= '0x'+c.join('');
+        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',1)';
+    }
+    throw new Error('Bad Hex');
 }
